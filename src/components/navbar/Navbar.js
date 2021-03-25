cce69fe7f1a30acb15e8324ba337ad6eb9e07d6f
Abhinav Gautam <abnvfloyd@gmail.com>
2021-03-25 19:49:51 +0545

    navbar done

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, Menu, MenuItem, Typography} from '@material-ui/core';
+import  {ShoppingCart} from '@material-ui/icons';
+
+import logo from '../../assets/ecommerce.png';
+import useStyles from './styles.js';
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="E-commerce" className={classes.image} height="25px"/>
+                        E-commerce
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show cart items" color="inherit">
+                                <Badge badgeContent={2} color="secondary">
+                                    <ShoppingCart />
+                                </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Navbar
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Grid } from '@material-ui/core';
 import Product from './product/Product';
 
+import useStyles from './styles';
+
 const products =[
     {
         id: 1,
@@ -20,8 +22,10 @@ const products =[
 ];
 
 const Products = () => {
+    const classes = useStyles();
     return(
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
         <Grid container justify="center" sapcing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
