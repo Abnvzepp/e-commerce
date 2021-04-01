6dbf34ce106690c9fd8cdc07f2c4fe72fafe1ef4
Abhinav Gautam <abnvfloyd@gmail.com>
2021-04-01 22:28:23 +0545

    subdivision completed

--- src/components/checkoutForm/checkout/Checkout.js
+++ src/components/checkoutForm/checkout/Checkout.js
@@ -12,6 +12,7 @@ const Checkout = ({ cart }) => {
 
     const [activeStep, setActiveStep] = useState(0);
     const[checkoutToken, setCheckoutToken] = useState(null);
+    const[shippingData, setShippingData] = useState({});
 
     useEffect(() => {
         const generateToken = async() =>{
@@ -23,9 +24,17 @@ const Checkout = ({ cart }) => {
             }
         }
         generateToken();
-    }, []);
+    }, [cart]);
 
-    const Form =() => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} /> : <PaymentForm />
+    const nextStep =() => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    const backStep =() => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+    const next =(data) => {
+        setShippingData(data);
+        nextStep();
+    }
+
+    const Form =() => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} next={next} /> : <PaymentForm checkoutToken={checkoutToken} />
 
     const Confirmation =() => ( <div>Confirmation</div>)
 
