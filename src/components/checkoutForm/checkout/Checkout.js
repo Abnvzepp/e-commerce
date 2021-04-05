f50cfdc99e6fb82b41609e165655af49d0726df7
Abhinav Gautam <abnvfloyd@gmail.com>
2021-04-05 18:53:15 +0545

    integration done

--- src/components/checkoutForm/checkout/Checkout.js
+++ src/components/checkoutForm/checkout/Checkout.js
@@ -8,7 +8,7 @@ import useStyles from './styles.js';
 
 const steps = ['Shipping Address', 'Payment details'];
 
-const Checkout = ({ cart, order, errorMsg, onCaptureCheckout }) => {
+const Checkout = ({ cart, order, error, onCaptureCheckout }) => {
     const classes = useStyles();
 
     const [activeStep, setActiveStep] = useState(0);
@@ -21,7 +21,7 @@ const Checkout = ({ cart, order, errorMsg, onCaptureCheckout }) => {
                 const token = await commerce.checkout.generateToken(cart.id, { type:'cart'});
                 setCheckoutToken(token);
             } catch (error) {
-                
+                console.log(error);
             }
         }
         generateToken();
@@ -37,17 +37,29 @@ const Checkout = ({ cart, order, errorMsg, onCaptureCheckout }) => {
 
     const Form =() => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} next={next} /> : <PaymentForm checkoutToken={checkoutToken} backStep={backStep} nextStep={nextStep} onCaptureCheckout={onCaptureCheckout} shippingData={shippingData}/>
 
-    const Confirmation =() => ( 
+    let Confirmation =() => order.customer ? ( 
         <>
             <div>
-                <Typography variant="h5">Thank You for your purchase, firstName lastName</Typography>
+                <Typography variant="h5">Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}</Typography>
                 <Divider className={classes.divider}/>
-                <Typography variant="subtitle2">Order ref: ref</Typography>
+                <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
             </div>
             <br />
             <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
         </>
-    )
+    ): (
+        <div className={classes.spinner}>
+            <CircularProgress />
+        </div>
+    );
+
+    if(error) {
+        <>
+            <Typography variant="h5">Error: {error}</Typography>
+            <br />
+            <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
+        </>
+    }
 
     return (
         <div>
